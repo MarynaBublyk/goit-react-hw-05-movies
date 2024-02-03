def59655898bf157a9e47a74f2abec683c811457
MarynaBublyk <137985327+MarynaBublyk@users.noreply.github.com>
2024-02-04 00:09:16 +0200

    DZ_5/1

---
+++ src/Error/Error.jsx
@@ -0,0 +1,11 @@
+export const PageNotFound = () => {
+  return <div>Opps!!!</div>;
+};
+
+export const NoReviews = () => {
+  return <p>Sorry, we don't have any reviews on this movie</p>;
+};
+
+export const NoCast = () => {
+  return <p>Sorry, we don't have any cast on this movie</p>;
+};
